5ca788f05ad507ab0e1ef405a0f69fc92da039f2
fumimi <info@fumimi.jp>
2018-01-03 23:50:36 +0900

    Initial Commit

---
+++ SmartyNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartyNews
+//
+//  Created by Takafumi Sato on 2018/01/03.
+//  Copyright © 2018年 Takafumi Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
